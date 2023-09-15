f9cb6cd81aa4332f629917e5b5436c38e81e76ba
Valentin Kuznetsov <vkuznet@gmail.com>
2023-09-15 09:41:33 -0400

    Add POST method to post meta-data records

--- meta.go
+++ meta.go
@@ -2,31 +2,40 @@ package main
 
 // MetaData represents meta-data object
 type MetaData struct {
-	Site        string   `json:"site"`
-	Description string   `json:"description"`
+	Site        string   `json:"site binding:"required""`
+	Description string   `json:"description" binding:"required"`
 	Tags        []string `json:"tags"`
 }
 
+// global list of existing meta-data records
+// should be replaced with permistent MongoDB storage
+var _metaData []MetaData
+
 // helper function to return existing meta-data
 func metadata() []MetaData {
-	var out []MetaData
-	data := MetaData{
-		Site:        "SiteA",
-		Description: "this site provides access to mineral waste",
-		Tags:        []string{"waste", "minerals"},
-	}
-	out = append(out, data)
-	data = MetaData{
-		Site:        "SiteB",
-		Description: "this site provides access to metal waste",
-		Tags:        []string{"waste", "metal"},
-	}
-	out = append(out, data)
-	data = MetaData{
-		Site:        "SiteC",
-		Description: "this site provides access to glass waste",
-		Tags:        []string{"waste", "glass"},
-	}
-	out = append(out, data)
-	return out
+	// example of adding meta-data
+	/*
+		var out []MetaData
+		data := MetaData{
+			Site:        "SiteA",
+			Description: "this site provides access to mineral waste",
+			Tags:        []string{"waste", "minerals"},
+		}
+		out = append(out, data)
+		data = MetaData{
+			Site:        "SiteB",
+			Description: "this site provides access to metal waste",
+			Tags:        []string{"waste", "metal"},
+		}
+		out = append(out, data)
+		data = MetaData{
+			Site:        "SiteC",
+			Description: "this site provides access to glass waste",
+			Tags:        []string{"waste", "glass"},
+		}
+		out = append(out, data)
+		return out
+	*/
+	// so far we will return our global _metaData list
+	return _metaData
 }
--- server.go
+++ server.go
@@ -15,6 +15,16 @@ func setupRouter() *gin.Engine {
 		data := metadata()
 		c.AsciiJSON(http.StatusOK, data)
 	})
+	r.POST("/meta", func(c *gin.Context) {
+		var meta MetaData
+		err := c.BindJSON(&meta)
+		if err == nil {
+			_metaData = append(_metaData, meta)
+			c.JSON(200, gin.H{"status": "ok"})
+		} else {
+			c.JSON(400, gin.H{"status": "fail", "error": err.Error()})
+		}
+	})
 
 	return r
 }
