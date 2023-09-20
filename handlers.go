5b5cfe2eef3a8134c5710c3d92b452e52f3bb6a2
Valentin Kuznetsov <vkuznet@gmail.com>
2023-09-20 13:13:03 -0400

    refactor handlers

--- handlers.go
+++ handlers.go
@@ -2,23 +2,33 @@ package main
 
 import (
 	"encoding/hex"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 )
 
+// SiteParams represents URI storage params in /meta end-point
+type SiteParams struct {
+	Site string `uri:"site" binding:"required"`
+}
+
 // MetaHandler provives access to GET /meta end-point
 func MetaHandler(c *gin.Context) {
 	data := metadata("")
-	c.AsciiJSON(http.StatusOK, data)
+	//     c.AsciiJSON(http.StatusOK, data)
+	c.JSON(200, gin.H{"status": "ok", "data": data})
 }
 
 // MetaSiteHandler provides access to GET /meta/:site end-point
 func MetaSiteHandler(c *gin.Context) {
-	site := c.Param("site")
-	data := metadata(site)
-	c.AsciiJSON(http.StatusOK, data)
+	var params SiteParams
+	if err := c.ShouldBindUri(&params); err == nil {
+		data := metadata(params.Site)
+		//         c.AsciiJSON(http.StatusOK, data)
+		c.JSON(200, gin.H{"status": "ok", "data": data})
+	} else {
+		c.JSON(400, gin.H{"status": "fail", "error": err.Error()})
+	}
 }
 
 // MetaPostHandler provides access to POST /meta end-point
--- meta.go
+++ meta.go
@@ -1,5 +1,7 @@
 package main
 
+import "log"
+
 // MetaData represents meta-data object
 type MetaData struct {
 	ID          string   `json:"id"`
@@ -15,35 +17,18 @@ var _metaData []MetaData
 
 // helper function to return existing meta-data
 func metadata(site string) []MetaData {
-	// example of adding meta-data
-	/*
-		var out []MetaData
-		data := MetaData{
-			Site:        "SiteA",
-			Description: "this site provides access to mineral waste",
-			Tags:        []string{"waste", "minerals"},
-		}
-		out = append(out, data)
-		data = MetaData{
-			Site:        "SiteB",
-			Description: "this site provides access to metal waste",
-			Tags:        []string{"waste", "metal"},
-		}
-		out = append(out, data)
-		data = MetaData{
-			Site:        "SiteC",
-			Description: "this site provides access to glass waste",
-			Tags:        []string{"waste", "glass"},
-		}
-		out = append(out, data)
-		return out
-	*/
 	// so far we will return our global _metaData list
+	if Config.Verbose > 0 {
+		log.Println("metadata for site=", site)
+	}
 	if site == "" {
 		return _metaData
 	}
 	var out []MetaData
 	for _, r := range _metaData {
+		if Config.Verbose > 0 {
+			log.Printf("MetaData record %+v matching site %s", r, site)
+		}
 		if r.Site == site {
 			out = append(out, r)
 		}
